b0e652fd638f675e9ad6e59b4f4c976760a1140f
mamootrahal <36728380+mamootrahal@users.noreply.github.com>
2025-12-23 21:17:21 +0300

    cosd to be finished

--- np.py
+++ np.py
@@ -1,35 +1,5 @@
 import numpy as np
 
-# a = np.array([[1, 2],
-#               [3, 4]])   # shape (2, 2)
-
-# b = np.array([[5, 6],
-#               [7, 8]])   # shape (2, 2)
-
-
-# print(np.concatenate([a, b], axis=0), type(np.concatenate([a, b], axis=0)))
-# # shape станет (4, 2)
-
-# print(np.concatenate([a, b], axis=1))
-# # shape станет (2, 4)
-# print('_' * 20)
-
-# a = np.array([[1, 2, 3], 
-#               [4, 5, 6]])
-# print(a.shape)
-
-# 8*5
-# 0 0 1 0 0 
-# 0 1 0 0 0 
-# 0 1 1 1 1 
-# 0 1 0 0 0 
-# 1 1 0 1 0 
-# 1 1 1 0 1 
-# 1 1 1 0 0 
-# 1 0 0 1 1 
-
-c = 0
-
 data = []
 while True:
     try:
@@ -41,11 +11,3 @@ while True:
         data.append(list(map(int, line.split())))
 
 A = np.array(data, dtype=int)
-
-n = A.shape[1]
-
-for i in range(n):
-    if all(A[:, i] == 0):
-        c += 1
-
-print(c)
