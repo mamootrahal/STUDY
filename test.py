1e6db3184c62c28f367fa493d63fa383e93681ad
mamootrahal <36728380+mamootrahal@users.noreply.github.com>
2025-12-23 19:47:11 +0300

    A. [np] Нулевые столбцы

--- test.py
+++ test.py
@@ -1,26 +1,6 @@
-s = list(map(str, input()))
+lines = [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]]
 
-pairs = {')': '(', ']': '[', '}': '{', '>': '<'}
-brackets = list(pairs.keys()) + list(pairs.values())
 
-st = []
-ok = True
-
-for i in range(len(s)):
-    ch = s[i]
-
-    if ch in pairs.values():          # открывающая
-        st.append(ch)
-    elif ch in pairs.keys():          # закрывающая
-        if len(st) == 0 or st[-1] != pairs[ch]:
-            ok = False
-            break
-        st.pop()
-    else:
-        ok = False
-        break
-
-if ok and len(st) == 0:
-    print("YES")
-else:
-    print("NO")
+print(lines[1:])
