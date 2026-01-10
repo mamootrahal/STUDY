1abd8943ea0ae9b60792c3f65108214df50c801c
mamootrahal <36728380+mamootrahal@users.noreply.github.com>
2026-01-10 14:14:59 +0300

    isNan

--- pd0.py
+++ pd0.py
@@ -3,11 +3,12 @@ import numpy as np
 import sys
 from io import StringIO
 
-data = sys.stdin.read().strip()
-if data:
-    df = pd.read_csv(StringIO(data))
-else:
-    df = pd.read_csv("input.csv")  # stdin — это “файл”, pandas умеет
+# data = sys.stdin.read().strip()
+# if data:
+#     df = pd.read_csv(StringIO(data))
+# else:
+df = pd.read_csv("input.csv")  # stdin — это “файл”, pandas умеет
+
 # df =  pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9], "d": [10, 11, 12], "e": [13, 14, 15]})  # читать из него напрямую
 # print(df)
 # print("Первые строки", df.head(5),       # первые строки
@@ -19,13 +20,12 @@ else:
 # "статистика по числам", df.describe())   # статистика по числам
 #print(df.columns.tolist())
 #print([repr(c) for c in df.columns])
+c = {"Имя" : 0, "Возраст" : 0, "Балл" : 0}
 
-counts = df["Имя"].value_counts()
-print("counts:", counts)
-mx = counts.max()
-print("mx:", mx)
-top = counts[counts == mx].sort_index()   # алфавитный порядок
-print("top:", top)
+for x in df:
+    for j in range(len(df.loc[:, x])):
+        val = df.loc[:, x].iloc[j]
+        if pd.isna(val): 
+            c[x] += 1
 
-for name, c in top.items():
-    print(name, c)
+print(pd.DataFrame(c.items(), columns=['Название_колонки', 'Количество_NaN']).to_csv(index=False))
